85c8e935b16ab1e9423d65ebec517f21bb507774
ggoraa <ggoraa1029@gmail.com>
2021-09-29 09:25:05 +0300

    added hasDestructiveAction argument to ButtonItem, idented the file with tabs

--- src/main/kotlin/dev/ggoraa/compose/touchbar/ButtonItem.kt
+++ src/main/kotlin/dev/ggoraa/compose/touchbar/ButtonItem.kt
@@ -1,3 +1,5 @@
+@file:Suppress("FunctionName")
+
 package dev.ggoraa.compose.touchbar
 
 import androidx.compose.runtime.Composable
@@ -6,48 +8,58 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.graphics.Color
 
 @Composable fun TouchBarScope.ButtonItem(
-  title: String,
-  alternateTitle: String = "",
-  image: TouchBarImage? = null,
-  alternateImage: TouchBarImage? = null,
-  bezelColor: Color? = null,
-  onClick: () -> Unit,
+	/** The title displayed on the button when it’s in an off state. */
+	title: String,
+	alternateTitle: String = "",
+	image: TouchBarImage? = null,
+	alternateImage: TouchBarImage? = null,
+	bezelColor: Color? = null,
+	/** A Boolean value that defines whether a button’s action has a destructive effect. */
+	hasDestructiveAction: Boolean = false,
+	onClick: () -> Unit,
 ) {
-  val updatedOnClick by rememberUpdatedState(onClick)
+	val updatedOnClick by rememberUpdatedState(onClick)
 
-  TouchBarViewNode(
-    viewFactory = {
-      val actionTarget = object : GenericActionTarget() {
-        override fun triggerNullary() {
-          updatedOnClick()
-        }
-      }
+	TouchBarViewNode(
+		viewFactory = {
+			val actionTarget = object : GenericActionTarget() {
+				override fun triggerNullary() {
+					updatedOnClick()
+				}
+			}
 
-      client.sendProxy(
-        "NSButton", "buttonWithTitle:target:action:",
-        /* title */ title,
-        /* target */ actionTarget,
-        /* action(selector) */ "triggerNullary"
-      )
-    },
-    update = {
-      set(title) {
-        viewImpl!!.send("setStringValue:", it)
-      }
-      set(alternateTitle) {
-        viewImpl!!["alternateTitle"] = it
-      }
-      set(image) {
-        viewImpl!!["image"] = it
-      }
-      set(alternateImage) {
-        viewImpl!!["alternateImage"] = it
-      }
-      set(bezelColor) {
-        it?.let { color ->
-          viewImpl!!["bezelColor"] = color.toNSColor()
-        }
-      }
-    }
-  )
+			client.sendProxy(
+				"NSButton", "buttonWithTitle:target:action:",
+				/* title */ title,
+				/* target */ actionTarget,
+				/* action(selector) */ "triggerNullary"
+			)
+		},
+		update = {
+			set(title) {
+				viewImpl!!["title"] = it
+			}
+			set(alternateTitle) {
+				viewImpl!!["alternateTitle"] = it
+			}
+			set(image) {
+				if (it != null) {
+					viewImpl!!["image"] = it.nsImage
+				}
+			}
+			set(alternateImage) {
+				if (it != null) {
+					viewImpl!!["alternateImage"] = it.nsImage
+				}
+			}
+			set(bezelColor) {
+				it?.let { color ->
+					viewImpl!!["bezelColor"] = color.toNSColor()
+				}
+			}
+			set(hasDestructiveAction) {
+				viewImpl!!["hasDestructiveAction"] = it
+			}
+		}
+	)
 }
