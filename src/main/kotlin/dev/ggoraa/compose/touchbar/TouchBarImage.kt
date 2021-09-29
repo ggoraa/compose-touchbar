ac620acac7c1aa92022250fb157d4ade8ee64e9f
ggoraa <ggoraa1029@gmail.com>
2021-09-29 09:25:49 +0300

    rewritten TouchBarImage

--- src/main/kotlin/dev/ggoraa/compose/touchbar/TouchBarImage.kt
+++ src/main/kotlin/dev/ggoraa/compose/touchbar/TouchBarImage.kt
@@ -1,8 +1,14 @@
+@file:Suppress("FunctionName")
+
 package dev.ggoraa.compose.touchbar
 
+import ca.weblite.objc.Client
 import ca.weblite.objc.Proxy
 
-class TouchBarImage internal constructor(internal val nsImage: Proxy) {
-  override fun equals(other: Any?): Boolean = (other as? TouchBarImage)?.nsImage == nsImage
-  override fun hashCode(): Int = nsImage.hashCode()
+fun TouchBarSystemImage(name: String, accessibilityDescription: String): TouchBarImage  {
+  val client = Client.getInstance()
+  return TouchBarImage(client.sendProxy("NSImage", "imageWithSystemSymbolName:accessibilityDescription:", name, accessibilityDescription))
 }
+
+data class TouchBarImage(val nsImage: Proxy)
+
